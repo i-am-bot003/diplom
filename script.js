bc48e70955eff84d23d40673ea76df34b1986abc
i-am-bot003 <82062758+i-am-bot003@users.noreply.github.com>
2023-04-04 19:10:35 +0300

    adopt

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function num(){
 var slideIndex = 1;
 showSlides(slideIndex);
 
+
 // Вперед/назад элементы управления
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -24,7 +25,11 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
+      var cursortext = document.getElementById('labelcursor');
+      var cursor = document.getElementById('cursor');
       slides[i].style.display = "none";
+      cursor.style.display = "none"
+      cursortext.style.display = "none"
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
