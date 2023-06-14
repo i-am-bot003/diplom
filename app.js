394c40e67c3358e313315a4a625d6434120f70a6
i-am-bot003 <82062758+i-am-bot003@users.noreply.github.com>
2023-06-14 04:20:25 +0300

    update test

---
+++ app.js
@@ -0,0 +1,7 @@
+function send(item, people, car, options, numtel, comment){
+    let data = item + ', ' + people + ', ' + car + ', ' + options + ', ' + numtel + ', ' + comment
+
+    
+    console.log(data)
+}
+
--- send.php
+++ send.php
@@ -1,17 +1,22 @@
 <?php
-$fio = $_POST['fio'];
-$email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-$fio = urldecode($fio);
-$email = urldecode($email);
-$fio = trim($fio);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
- {     echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
+        header("Access-Control-Allow-Origin: *");
+        $to = "shtorm_and_happy_enderman@mail.ru";
+        $subject = 'Заказ'; //Заголовок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>
+                        <p>Email: '.$_POST['email'].'</p>
+                        <p>Регион: '.$_POST['region'].'</p>
+                        <p>Сообщение: '.$_POST['text'].'</p>                                   
+                    </body>
+                </html>'; //Текст сообщения
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+
+?>
